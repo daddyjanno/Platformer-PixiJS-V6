c996658f39e4af841489b469336e7da2148fa349
daddyjanno <jn.drugmand@gmail.com>
2024-11-08 18:11:28 +0100

    physics with Matter and destroy method

--- src/scripts/game/GameScene.js
+++ src/scripts/game/GameScene.js
@@ -1,9 +1,10 @@
 import * as PIXI from 'pixi.js'
 import { Scene } from '../system/Scene'
 import { Background } from './Background'
-import { Platform } from './Platform'
 import { Hero } from './Hero'
 import { Platforms } from './Platforms'
+import Matter from 'matter-js'
+import { App } from '../system/App'
 
 export class GameScene extends Scene {
     constructor() {
@@ -13,8 +14,9 @@ export class GameScene extends Scene {
     }
     create() {
         this.createBackground()
-        this.createPlatforms()
         this.createHero()
+        this.createPlatforms()
+        this.setEvents()
     }
     createBackground() {
         this.bg = new Background()
@@ -24,6 +26,14 @@ export class GameScene extends Scene {
     createHero() {
         this.hero = new Hero()
         this.container.addChild(this.hero.sprite)
+
+        this.container.interactive = true
+        this.container.on('pointerdown', () => {
+            this.hero.startJump()
+        })
+        this.hero.sprite.once('die', () => {
+            App.scenes.start('Game')
+        })
     }
     createPlatforms() {
         this.platforms = new Platforms()
@@ -33,5 +43,32 @@ export class GameScene extends Scene {
     update(dt) {
         this.bg.update(dt)
         this.platforms.update(dt)
+    }
+    setEvents() {
+        Matter.Events.on(
+            App.physics,
+            'collisionStart',
+            this.onCollisionStart.bind(this)
+        )
+    }
+    onCollisionStart(event) {
+        const colliders = [event.pairs[0].bodyA, event.pairs[0].bodyB]
+        const hero = colliders.find((body) => body.gameHero)
+        const platform = colliders.find((body) => body.gamePlatform)
+
+        if (hero && platform) {
+            this.hero.stayOnPlatform(platform.gamePlatform)
+        }
+    }
+    destroy() {
+        Matter.Events.off(
+            App.physics,
+            'collisionStart',
+            this.onCollisionStart.bind(this)
+        )
+        App.app.ticker.remove(this.update, this)
+        this.bg.destroy()
+        this.hero.destroy()
+        this.platforms.destroy()
     }
 }
