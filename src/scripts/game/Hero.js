223fe59a4833060629197b4af245eb274295637c
daddyjanno <jn.drugmand@gmail.com>
2024-11-08 18:10:43 +0100

    Hero

--- src/scripts/game/Hero.js
+++ src/scripts/game/Hero.js
@@ -1,10 +1,54 @@
 import * as PIXI from 'pixi.js'
+import * as Matter from 'matter-js'
 import { App } from '../system/App'
 
 export class Hero {
     constructor() {
         this.createSprite()
+        this.createBody()
+
+        App.app.ticker.add(this.update, this)
+
+        this.dy = App.config.hero.jumpSpeed
+        this.maxJumps = App.config.hero.maxJumps
+        this.jumpIndex = 0
+        this.score = 0
     }
+    startJump() {
+        if (this.platform || this.jumpIndex === 1) {
+            ++this.jumpIndex
+            this.platform = null
+            Matter.Body.setVelocity(this.body, { x: 0, y: -this.dy })
+        }
+    }
+    stayOnPlatform(platform) {
+        this.platform = platform
+        this.jumpIndex = 0
+    }
+
+    createBody() {
+        this.body = Matter.Bodies.rectangle(
+            this.sprite.x + this.sprite.width / 2,
+            this.sprite.y + this.sprite.height / 2,
+            this.sprite.width,
+            this.sprite.height,
+            { friction: 0 }
+        )
+        Matter.World.add(App.physics.world, this.body)
+        this.body.gameHero = this
+    }
+
+    update() {
+        this.sprite.x = this.body.position.x - this.sprite.width / 2
+        this.sprite.y = this.body.position.y - this.sprite.height / 2
+
+        console.log(this.body.position.x, this.body.position.y)
+
+        if (this.sprite.y > window.innerHeight) {
+            this.sprite.emit('die')
+        }
+    }
+
     createSprite() {
         this.sprite = new PIXI.AnimatedSprite([
             App.res('walk1'),
@@ -16,5 +60,10 @@ export class Hero {
         this.sprite.loop = true
         this.sprite.animationSpeed = 0.1
         this.sprite.play()
+    }
+    destroy() {
+        App.app.ticker.remove(this.update, this)
+        Matter.World.add(App.physics.world, this.body)
+        this.sprite.destroy()
     }
 }
